5c18ba2c9ffd403dfede36563076684522fb71ec
archkrish21 <65929765+archkrish21@users.noreply.github.com>
2026-06-23 01:02:20 -0500

    Archkrish21 patch 1 (#176)
    * Integrate Sentry replay functionality
    
    Added Sentry replay integration with configuration options.
    
    * Configure network details in Sentry client
    
    Add network detail configuration for Sentry client.
    
    * Refactor Sentry replay integration settings
    
    Updated Sentry replay integration configuration to include detailed options for masking and network request/response headers.
    
    * Refactor Sentry initialization configuration

--- saps/sentry.client.config.ts
+++ saps/sentry.client.config.ts
@@ -1,25 +1,36 @@
 // This file configures the initialization of Sentry in the browser.
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
+  
 import * as Sentry from "@sentry/nextjs";
 
 if (!process.env.NEXT_PUBLIC_E2E_DISABLE_TELEMETRY) {
-Sentry.init({
-  dsn: "https://fc2aa07e939254dd4db1d69fae31d2d1@o4511225622953984.ingest.us.sentry.io/4511225634488320",
+  Sentry.init({
+    dsn: "https://fc2aa07e939254dd4db1d69fae31d2d1@o4511225622953984.ingest.us.sentry.io/4511225634488320",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production.
-  replaysSessionSampleRate: 0.1,
+    // Define how likely Replay events are sampled.
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production.
+    replaysSessionSampleRate: 0.1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+    // Define how likely Replay events are sampled when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+
+    integrations: [
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+        maskAllInputs: false,
+        networkDetailAllowUrls: ['/api/'],
+        networkRequestHeaders: ['x-custom-header'],
+        networkResponseHeaders: ['content-type'],
+      }),
+    ],
+  });
 }

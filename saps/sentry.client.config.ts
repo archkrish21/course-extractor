9cfd8d7f9f0d2f68dd8a0cef7f43cc913f05af7f
archkrish21 <65929765+archkrish21@users.noreply.github.com>
2026-06-23 00:48:54 -0500

    Refactor Sentry initialization configuration

--- saps/sentry.client.config.ts
+++ saps/sentry.client.config.ts
@@ -1,28 +1,28 @@
 // This file configures the initialization of Sentry in the browser.
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
+  
 import * as Sentry from "@sentry/nextjs";
 
 if (!process.env.NEXT_PUBLIC_E2E_DISABLE_TELEMETRY) {
-Sentry.init({
-  dsn: "https://fc2aa07e939254dd4db1d69fae31d2d1@o4511225622953984.ingest.us.sentry.io/4511225634488320",
+  Sentry.init({
+    dsn: "https://fc2aa07e939254dd4db1d69fae31d2d1@o4511225622953984.ingest.us.sentry.io/4511225634488320",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production.
-  replaysSessionSampleRate: 0.1,
+    // Define how likely Replay events are sampled.
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production.
+    replaysSessionSampleRate: 0.1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+    // Define how likely Replay events are sampled when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  integrations: [
+    integrations: [
       Sentry.replayIntegration({
         maskAllText: false,
         blockAllMedia: false,
@@ -32,4 +32,5 @@ Sentry.init({
         networkResponseHeaders: ['content-type'],
       }),
     ],
-});
+  });
+}
